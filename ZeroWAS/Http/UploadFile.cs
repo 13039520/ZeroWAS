cc8013237242e0bec307e5e96d5bbcbb5e370023
ruzi <735753456@qq.com>
2026-01-13 16:38:17 +0800

    Update
    Update

--- ZeroWAS/Http/UploadFile.cs
+++ ZeroWAS/Http/UploadFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ZeroWAS.Http
@@ -11,12 +12,11 @@ namespace ZeroWAS.Http
         private long _ContentLength;
         private string _ContentType;
         private string _FileName;
+        private bool _reading = false;
 
         public string FileName { get { return _FileName; } }
         public string ContentType { get { return _ContentType; } }
         public long ContentLength { get { return _ContentLength; } }
-
-
 
         public UploadFile(System.IO.Stream _RawStream, long _InRawStreamIndex, long _ContentLength, string _ContentType, string _FileName)
         {
@@ -26,47 +26,45 @@ namespace ZeroWAS.Http
             this._ContentType = _ContentType;
             this._FileName = _FileName;
         }
-
-
         public void SaveAs(string filePath)
         {
-            bool exists = System.IO.File.Exists(filePath);
             using (System.IO.FileStream fs = new System.IO.FileStream(
                 filePath,
-                exists ? System.IO.FileMode.Truncate : System.IO.FileMode.Create,
+                System.IO.FileMode.Create,
                 System.IO.FileAccess.Write,
                 System.IO.FileShare.Read))
             {
-                fs.Position = 0;
-                if (_RawStream != null && _RawStream.CanRead && _ContentLength > 0 && _RawStream.Length > _ContentLength)
-                {
-                    long beginIndex = _InRawStreamIndex;
-                    long endIndex = _InRawStreamIndex + _ContentLength;
-                    long rLen = 2097152;//2M
-                    if (_ContentLength < rLen)
-                    {
-                        rLen = _ContentLength;
-                    }
-                    _RawStream.Position = beginIndex;
-
-                    while (beginIndex< endIndex)
-                    {
-                        byte[] buffer = new byte[rLen];
-                        int readLen = _RawStream.Read(buffer, 0, buffer.Length);
-                        if (readLen < 1) { break; }
-                        if (buffer.Length > readLen)
-                        {
-                            byte[] t = new byte[buffer.Length];
-                            Array.Copy(buffer, t, t.Length);
-                            buffer = t;
-                        }
-                        beginIndex += buffer.Length;
-                        fs.Write(buffer, 0, buffer.Length);
-
-                    }                    
-                }
-                fs.Dispose();
+                SaveTo(fs);
             }
+        }
+        public void SaveTo(Stream stream)
+        {
+            if (_reading) { throw new Exception("Cannot perform concurrent read/write operations"); }
+            if (!stream.CanWrite || !stream.CanSeek) { throw new Exception("The current stream does not support writing."); }
+            _reading = true;
+            stream.Position = 0;
+            if (_RawStream != null && _RawStream.CanRead && _ContentLength > 0 && _RawStream.Length > _ContentLength)
+            {
+                long beginIndex = _InRawStreamIndex;
+                long endIndex = _InRawStreamIndex + _ContentLength;
+                long rLen = 2097152;//2M
+                if (_ContentLength < rLen)
+                {
+                    rLen = _ContentLength;
+                }
+                _RawStream.Position = beginIndex;
+
+                while (beginIndex < endIndex)
+                {
+                    byte[] buffer = new byte[rLen];
+                    int readLen = _RawStream.Read(buffer, 0, buffer.Length);
+                    if (readLen < 1) { break; }
+                    beginIndex += readLen;
+                    stream.Write(buffer, 0, readLen);
+                }
+            }
+            stream.Position = 0;
+            _reading = false;
         }
     }
 }
